e647008cd7e82da96f1d32a09c60747494a7ab80
Nate Palmore <nnpalmore@gmail.com>
2022-09-13 10:16:16 -0700

    initial commit

---
+++ server.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+
+app.secret_key="order66"
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/process', methods=['POST'])
+def create_user():
+    session['username'] = request.form['name']
+    session['userlocation'] = request.form['location']
+    session['userlanguage'] = request.form['language']
+    session['usercomment'] = request.form['comment']
+    return redirect('/result')
+
+@app.route("/result")
+def show_user():
+    return render_template('result.html')
+
+@app.route('/destroy_session')
+def reset():
+    session.clear()
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
